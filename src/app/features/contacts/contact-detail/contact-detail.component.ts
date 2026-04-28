3a0d80cd77912e4a1f460c3c0cb15c8455586f17
Andre Funk <Andre.funk.94@web.de>
2026-04-29 00:48:20 +0200

    feat(contact-detail: add initial letters & cta´s (delete and edit)

--- src/app/features/contacts/contact-detail/contact-detail.component.ts
+++ src/app/features/contacts/contact-detail/contact-detail.component.ts
@@ -1,16 +1,17 @@
 import { Component, effect, inject, input, signal } from '@angular/core';
 import { Contact, ContactsService } from '../contacts.service';
 import { LinkComponent } from '@shared/ui/link/link.component';
+import { InitialLetterComponent } from '@shared/ui/initial-letter/initial-letter.component';
 
 @Component({
     selector: 'contacts-contact-detail',
-    imports: [LinkComponent],
+    imports: [LinkComponent, InitialLetterComponent],
     templateUrl: './contact-detail.component.html',
     styleUrl: './contact-detail.component.scss'
 })
 export class ContactDetailComponent {
-    contactService = inject(ContactsService);
-    contactID = input.required<number>();
+    private contactService = inject(ContactsService);
+    readonly contactID = input.required<number>();
 
     contact = signal<Contact | undefined>(undefined);
 
