bba4c89c91f31953015ad66698f53e13d3799e38
WeinbergSolution <pascalgiercke@gmail.com>
2026-04-29 20:34:39 +0200

    feat(help) Add: Structure, style

--- src/app/features/public/help/help.component.ts
+++ src/app/features/public/help/help.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { IconComponent } from '@shared/ui/icon/icon.component';
 import { LinkComponent } from '@shared/ui/link/link.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'public-help',
-    imports: [LinkComponent, IconComponent],
+    imports: [LinkComponent, IconComponent, RouterLink],
     templateUrl: './help.component.html',
     styleUrl: './help.component.scss'
 })
