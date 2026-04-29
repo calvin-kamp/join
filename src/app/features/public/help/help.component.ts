608e4c8a2257f77c86b1866d183cd57eeb1056e3
WeinbergSolution <pascalgiercke@gmail.com>
2026-04-29 20:51:25 +0200

    feat(help) Add: Arrow Link fuction, Breakingpoint change -- Done

--- src/app/features/public/help/help.component.ts
+++ src/app/features/public/help/help.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { IconComponent } from '@shared/ui/icon/icon.component';
 import { LinkComponent } from '@shared/ui/link/link.component';
-import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'public-help',
-    imports: [LinkComponent, IconComponent, RouterLink],
+    imports: [LinkComponent, IconComponent],
     templateUrl: './help.component.html',
     styleUrl: './help.component.scss'
 })
