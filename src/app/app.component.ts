ee25310a3c6a48d515c1b9112d978c51b00ad943
WeinbergSolution <pascalgiercke@gmail.com>
2026-04-29 17:32:50 +0200

    feat(leag-notice) Pull Request review Changes Second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LegalNoticeComponent } from '@features/public/legal-notice/legal-notice.component';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, LegalNoticeComponent],
+    imports: [RouterOutlet],
     templateUrl: './app.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
