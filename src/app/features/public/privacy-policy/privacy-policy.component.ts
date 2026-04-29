323ca401ac76e8c4f85770225b37223c6b77e273
WeinbergSolution <pascalgiercke@gmail.com>
2026-04-29 16:24:45 +0200

    feat(privacy-policy) Add structur, layout, responsiv

--- src/app/features/public/privacy-policy/privacy-policy.component.ts
+++ src/app/features/public/privacy-policy/privacy-policy.component.ts
@@ -1,8 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { IconComponent } from '@shared/ui/icon/icon.component';
+import { LinkComponent } from '@shared/ui/link/link.component';
 
 @Component({
     selector: 'public-privacy-policy',
-    imports: [],
+    imports: [LinkComponent, IconComponent],
     templateUrl: './privacy-policy.component.html',
     styleUrl: './privacy-policy.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush
