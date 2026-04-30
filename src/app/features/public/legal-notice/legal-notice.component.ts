8f9a587f5ea85c1618680f3983dfaf885cf94c84
WeinbergSolution <pascalgiercke@gmail.com>
2026-04-30 12:39:18 +0200

    feat(leag-notice) correction, Arrowback change <Ui link to <a link, Wrap mailto an Email adress

--- src/app/features/public/legal-notice/legal-notice.component.ts
+++ src/app/features/public/legal-notice/legal-notice.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { LinkComponent } from '@shared/ui/link/link.component';
 import { IconComponent } from '@shared/ui/icon/icon.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'public-legal-notice',
-    imports: [LinkComponent, IconComponent, IconComponent],
+    imports: [LinkComponent, IconComponent, IconComponent, RouterLink],
     templateUrl: './legal-notice.component.html',
     styleUrl: './legal-notice.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush
