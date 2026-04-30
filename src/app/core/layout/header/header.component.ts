67edfdfc8932deaaca3397f9bacbed90b9f404e3
Mouna Nitsche <mouna.nitsche02@googlemail.com>
2026-04-30 15:31:46 +0200

    feat(main-header): add main-header

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,10 +1,19 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { LogoComponent } from '@shared/ui/logo/logo.component';
+import { LinkComponent } from '@shared/ui/link/link.component';
+import { IconComponent } from '@shared/ui/icon/icon.component';
 
 @Component({
     selector: 'layout-header',
-    imports: [],
+    imports: [LogoComponent, LinkComponent, IconComponent],
     templateUrl: './header.component.html',
     styleUrl: './header.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+    isToggled = false;
+    showMenu() {
+        this.isToggled = !this.isToggled;
+        console.log(this.isToggled);
+    }
+}
