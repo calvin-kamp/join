9b68d98edb6ae9f3fff6e6cbe43e1b47e7c6ad2e
WeinbergSolution <pascalgiercke@gmail.com>
2026-04-30 12:43:59 +0200

    fix(privacy-policy) Arrow Visibility, Change Ui link to a Routerlink

--- src/app/features/public/privacy-policy/privacy-policy.component.ts
+++ src/app/features/public/privacy-policy/privacy-policy.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { IconComponent } from '@shared/ui/icon/icon.component';
 import { LinkComponent } from '@shared/ui/link/link.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'public-privacy-policy',
-    imports: [LinkComponent, IconComponent],
+    imports: [LinkComponent, IconComponent, RouterLink],
     templateUrl: './privacy-policy.component.html',
     styleUrl: './privacy-policy.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush
