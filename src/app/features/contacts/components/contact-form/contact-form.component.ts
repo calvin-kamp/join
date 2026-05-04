44cf1f3e7cc14ec8669ef8d34bcf2888d33ede64
Andre Funk <Andre.funk.94@web.de>
2026-05-04 20:09:54 +0200

    feat(contact-form): add close button

--- src/app/features/contacts/components/contact-form/contact-form.component.ts
+++ src/app/features/contacts/components/contact-form/contact-form.component.ts
@@ -11,6 +11,7 @@ import { InitialLetterComponent } from '@shared/ui/initial-letter/initial-letter
 import { toSignal } from '@angular/core/rxjs-interop';
 import { LogoComponent } from '@shared/ui/logo/logo.component';
 import { IconComponent } from '@shared/ui/icon/icon.component';
+import { LinkComponent } from '@shared/ui/link/link.component';
 
 type FormType = 'add' | 'edit';
 
@@ -23,7 +24,8 @@ type FormType = 'add' | 'edit';
         ButtonComponent,
         InitialLetterComponent,
         LogoComponent,
-        IconComponent
+        IconComponent,
+        LinkComponent
     ],
     templateUrl: './contact-form.component.html',
     styleUrl: './contact-form.component.scss'
