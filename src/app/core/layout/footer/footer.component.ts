ee7144a15a1a61e3903d5fc9ab0c5cda69de84e3
Mouna Nitsche <mouna.nitsche02@googlemail.com>
2026-05-04 23:15:04 +0200

    feat(main-footer): add footer with nav-bar with responsive design

--- src/app/core/layout/footer/footer.component.ts
+++ src/app/core/layout/footer/footer.component.ts
@@ -1,8 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { IconComponent } from '@shared/ui/icon/icon.component';
+import { LinkComponent } from '@shared/ui/link/link.component';
+import { LogoComponent } from '@shared/ui/logo/logo.component';
 
 @Component({
     selector: 'layout-footer',
-    imports: [],
+    imports: [IconComponent, LinkComponent, LogoComponent],
     templateUrl: './footer.component.html',
     styleUrl: './footer.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush
