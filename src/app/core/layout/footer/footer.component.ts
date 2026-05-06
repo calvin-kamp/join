eb62f32ced2ef350dc9f949b81771a6e7f5fe96f
Calvin Kamp <calvin.kamp6696@gmail.com>
2026-05-06 13:57:02 +0200

    feat(main-footer): cleanup footer markup and styles

--- src/app/core/layout/footer/footer.component.ts
+++ src/app/core/layout/footer/footer.component.ts
@@ -1,7 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { IconComponent } from '@shared/ui/icon/icon.component';
+import { IconComponent, type IconName } from '@shared/ui/icon/icon.component';
 import { LinkComponent } from '@shared/ui/link/link.component';
 import { LogoComponent } from '@shared/ui/logo/logo.component';
+
+interface NavLink {
+    href: string;
+    label: string;
+    iconName: IconName;
+    activeOptions: {
+        exact: boolean;
+    };
+}
 
 @Component({
     selector: 'layout-footer',
@@ -10,4 +19,39 @@ import { LogoComponent } from '@shared/ui/logo/logo.component';
     styleUrl: './footer.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FooterComponent {}
+export class FooterComponent {
+    protected readonly navigationLinks: NavLink[] = [
+        {
+            href: '/summary',
+            label: 'Summary',
+            iconName: 'nav-summary',
+            activeOptions: {
+                exact: true
+            }
+        },
+        {
+            href: '/add-task',
+            label: 'Add Task',
+            iconName: 'nav-add-task',
+            activeOptions: {
+                exact: true
+            }
+        },
+        {
+            href: '/board',
+            label: 'Board',
+            iconName: 'nav-board',
+            activeOptions: {
+                exact: true
+            }
+        },
+        {
+            href: '/contacts',
+            label: 'Contacts',
+            iconName: 'nav-contacts',
+            activeOptions: {
+                exact: false
+            }
+        }
+    ];
+}
