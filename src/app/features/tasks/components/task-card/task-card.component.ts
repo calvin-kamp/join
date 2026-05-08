2df9fe8a998195a71fe46c7ddcd71d0c8cb0f53d
Andre Funk <Andre.funk.94@web.de>
2026-05-08 13:00:12 +0200

    feat(task-card): add task card component
    - add progessbar color to tokens.scss

--- src/app/features/tasks/components/task-card/task-card.component.ts
+++ src/app/features/tasks/components/task-card/task-card.component.ts
@@ -1,9 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Task } from '@features/tasks/tasks.service';
+import { CardDirective } from '@shared/directives/card.directive';
+import { IconComponent, IconName } from '@shared/ui/icon/icon.component';
+import { InitialLetterComponent } from '@shared/ui/initial-letter/initial-letter.component';
 
 @Component({
     selector: 'tasks-task-card',
-    imports: [],
+    imports: [IconComponent, InitialLetterComponent, CardDirective],
     templateUrl: './task-card.component.html',
     styleUrl: './task-card.component.scss'
 })
-export class TaskCardComponent {}
+export class TaskCardComponent {
+    readonly task = input.required<Task>();
+
+    categoryModifierClass() {
+        const className = 'task-card__badge--';
+        const modifier = this.task().category.name.toLowerCase().replace(' ', '-');
+
+        return className + modifier;
+    }
+
+    subtasksDone(): number {
+        let count = 0;
+
+        for (const subtask of this.task().subtasks) {
+            if (subtask.status) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    getBadgeName(): IconName {
+        return ('badge-' + this.task().priority.name.toLowerCase()) as IconName;
+    }
+
+    setProgress(): number {
+        const subtasks = this.task().subtasks;
+
+        if (!subtasks.length) return 0;
+
+        let done = 0;
+
+        for (const task of subtasks) {
+            if (task.status) {
+                done++;
+            }
+        }
+
+        const percent = done / subtasks.length;
+
+        return percent * 110;
+    }
+
+    visibleContacts() {
+        return this.task().contacts.slice(0, 4);
+    }
+
+    hiddenContactsCount(): number {
+        const total = this.task().contacts.length;
+
+        return total > 4 ? total - 4 : 0;
+    }
+}
