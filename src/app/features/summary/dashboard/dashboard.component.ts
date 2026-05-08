4ddcee904787beb557f7f17c48349a164483f151
Mouna Nitsche <mouna.nitsche02@googlemail.com>
2026-05-08 13:50:45 +0200

    feat(summary): add dashboard component
    - icon updated to Icon component

--- src/app/features/summary/dashboard/dashboard.component.ts
+++ src/app/features/summary/dashboard/dashboard.component.ts
@@ -1,10 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { IconComponent } from '@shared/ui/icon/icon.component';
 
 @Component({
     selector: 'summary-dashboard',
-    imports: [],
+    imports: [IconComponent, IconComponent],
     templateUrl: './dashboard.component.html',
     styleUrl: './dashboard.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+    taskAmount = 16;
+}
