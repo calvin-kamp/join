465114799c8d75de46c3a648d11fdc8146b48203
Andre Funk <Andre.funk.94@web.de>
2026-06-02 17:34:02 +0200

    feat(task-detail): ui-checkbox add to task-detail and styles added
    - task-form changed icon

--- src/app/features/tasks/components/task-detail/task-detail.component.ts
+++ src/app/features/tasks/components/task-detail/task-detail.component.ts
@@ -1,15 +1,24 @@
-import { Component, input, output } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
 import { DatePipe } from '@angular/common';
 import { DialogComponent } from '@shared/ui/dialog/dialog.component';
 import { LinkComponent } from '@shared/ui/link/link.component';
 import { IconComponent, type IconName } from '@shared/ui/icon/icon.component';
 import { InitialLetterComponent } from '@shared/ui/initial-letter/initial-letter.component';
-import { type Task } from '@features/tasks/tasks.service';
+import { Subtask, TasksService, type Task } from '@features/tasks/tasks.service';
 import { ButtonComponent } from '@shared/ui/button/button.component';
+import { CheckboxComponent } from '@shared/ui/forms/checkbox/checkbox.component';
 
 @Component({
     selector: 'tasks-task-detail',
-    imports: [DialogComponent, LinkComponent, IconComponent, InitialLetterComponent, DatePipe, ButtonComponent],
+    imports: [
+        DialogComponent,
+        LinkComponent,
+        IconComponent,
+        InitialLetterComponent,
+        DatePipe,
+        ButtonComponent,
+        CheckboxComponent
+    ],
     templateUrl: './task-detail.component.html',
     styleUrl: './task-detail.component.scss'
 })
@@ -20,14 +29,22 @@ export class TaskDetailComponent {
     editRequested = output<Task>();
     deleteRequested = output<Task>();
 
+    private tasksService = inject(TasksService);
+
     priorityIcon(priorityName: string): IconName {
         return ('badge-' + priorityName.toLowerCase()) as IconName;
     }
 
-    categoryModifierClass(task: Task) {
+    categoryModifierClass(task: Task): string {
         const className = 'task-detail__badge--';
         const modifier = task.category.name.toLowerCase().replace(' ', '-');
 
         return className + modifier;
+    }
+
+    async onSubtaskChanged(subtask: Subtask, checked: boolean) {
+        await this.tasksService.updateSubtask(subtask.id, {
+            status: checked
+        });
     }
 }
